dee70931c9f20fd826c46f5df026aa37be408e35
hazratgs <hazratgs@gmail.com>
2016-11-13 16:36:31 +0300

    Начал страницу продуктов

---
+++ catalog.php
@@ -0,0 +1,96 @@
+<!doctype html>
+<html>
+<head>
+    <script src="public/js/project.js"></script>
+    <script>febox.init()</script>
+
+    <link rel="stylesheet" href="/public/css/style.css?ee=<?= rand(0, 999) ?>">
+</head>
+<body>
+<header class="white">
+    <div class="header-head">
+        <div class="header-head-wrapper">
+            <div class="header-head-wrapper-logo">
+                <div class="header-head-wrapper-logo-img"></div>
+                <div class="header-head-wrapper-logo-data">
+                    <h1>
+                        <strong>Ориент</strong>
+                        <span>Мебельные решения</span>
+                    </h1>
+                </div>
+            </div>
+            <div class="header-head-wrapper-nav">
+                <a href="#">О компании</a>
+                <a href="#">Кухни</a>
+                <a href="#">Шкафы-купе</a>
+                <a href="#">Материалы</a>
+                <a href="#">Оплата и доставка</a>
+                <a href="#">Контакты</a>
+            </div>
+            <div class="header-head-wrapper-contact">
+                <div class="header-head-wrapper-contact-phone">8 095 548 45 54</div>
+                <div class="header-head-wrapper-contact-social">
+                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Layer_1" x="0px" y="0px" width="512px" height="512px" viewBox="0 0 169.063 169.063" style="enable-background:new 0 0 169.063 169.063;" xml:space="preserve"><g><path d="M122.406,0H46.654C20.929,0,0,20.93,0,46.655v75.752c0,25.726,20.929,46.655,46.654,46.655h75.752   c25.727,0,46.656-20.93,46.656-46.655V46.655C169.063,20.93,148.133,0,122.406,0z M154.063,122.407   c0,17.455-14.201,31.655-31.656,31.655H46.654C29.2,154.063,15,139.862,15,122.407V46.655C15,29.201,29.2,15,46.654,15h75.752   c17.455,0,31.656,14.201,31.656,31.655V122.407z" fill="#FFFFFF"/><path d="M84.531,40.97c-24.021,0-43.563,19.542-43.563,43.563c0,24.02,19.542,43.561,43.563,43.561s43.563-19.541,43.563-43.561   C128.094,60.512,108.552,40.97,84.531,40.97z M84.531,113.093c-15.749,0-28.563-12.812-28.563-28.561   c0-15.75,12.813-28.563,28.563-28.563s28.563,12.813,28.563,28.563C113.094,100.281,100.28,113.093,84.531,113.093z" fill="#FFFFFF"/><path d="M129.921,28.251c-2.89,0-5.729,1.17-7.77,3.22c-2.051,2.04-3.23,4.88-3.23,7.78c0,2.891,1.18,5.73,3.23,7.78   c2.04,2.04,4.88,3.22,7.77,3.22c2.9,0,5.73-1.18,7.78-3.22c2.05-2.05,3.22-4.89,3.22-7.78c0-2.9-1.17-5.74-3.22-7.78   C135.661,29.421,132.821,28.251,129.921,28.251z" fill="#FFFFFF"/></g></svg>
+                    <strong>Мы в instagram</strong>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+<div class="page">
+    <div class="page-wrapper">
+        <div class="page-wrapper-head">
+            <div class="page-wrapper-head-crumbs">
+                <a href="#">Сайт</a>
+                <a href="#">Каталог</a>
+            </div>
+            <h1>Кухни</h1>
+        </div>
+
+    </div>
+</div>
+<footer>
+    <div class="footer-wrapper">
+        <div class="footer-wrapper-link">
+            <a href="#">О компании</a>
+            <a href="#">Кухни</a>
+            <a href="#">Шкафы-купе</a>
+            <a href="#">Материалы</a>
+            <a href="#">Оплата и доставка</a>
+            <a href="#">Контакты</a>
+        </div>
+        <div class="footer-wrapper-contact">
+            <a href="#">Перезвоните мне</a>
+        </div>
+    </div>
+    <div class="footer-data">
+        <div class="footer-data-logo">
+            <strong>Ориент</strong>
+            <span>Мебельные решения</span>
+
+            <p>© 2004-2016</p>
+
+            <div class="header-head-wrapper-contact-social">
+                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Layer_1" x="0px" y="0px" width="512px" height="512px" viewBox="0 0 169.063 169.063" style="enable-background:new 0 0 169.063 169.063;" xml:space="preserve"><g><path d="M122.406,0H46.654C20.929,0,0,20.93,0,46.655v75.752c0,25.726,20.929,46.655,46.654,46.655h75.752   c25.727,0,46.656-20.93,46.656-46.655V46.655C169.063,20.93,148.133,0,122.406,0z M154.063,122.407   c0,17.455-14.201,31.655-31.656,31.655H46.654C29.2,154.063,15,139.862,15,122.407V46.655C15,29.201,29.2,15,46.654,15h75.752   c17.455,0,31.656,14.201,31.656,31.655V122.407z" fill="#FFFFFF"></path><path d="M84.531,40.97c-24.021,0-43.563,19.542-43.563,43.563c0,24.02,19.542,43.561,43.563,43.561s43.563-19.541,43.563-43.561   C128.094,60.512,108.552,40.97,84.531,40.97z M84.531,113.093c-15.749,0-28.563-12.812-28.563-28.561   c0-15.75,12.813-28.563,28.563-28.563s28.563,12.813,28.563,28.563C113.094,100.281,100.28,113.093,84.531,113.093z" fill="#FFFFFF"></path><path d="M129.921,28.251c-2.89,0-5.729,1.17-7.77,3.22c-2.051,2.04-3.23,4.88-3.23,7.78c0,2.891,1.18,5.73,3.23,7.78   c2.04,2.04,4.88,3.22,7.77,3.22c2.9,0,5.73-1.18,7.78-3.22c2.05-2.05,3.22-4.89,3.22-7.78c0-2.9-1.17-5.74-3.22-7.78   C135.661,29.421,132.821,28.251,129.921,28.251z" fill="#FFFFFF"></path></g></svg>
+                <strong>Мы в instagram</strong>
+            </div>
+        </div>
+        <div class="footer-data-contact">
+            <span>тел.: +7 (495) 933-10-55,</span>
+            <span>Москва, Бережковская набережная,</span>
+            <span>д. 16а, БЦ «Riverside Stadion»</span>
+        </div>
+        <div class="footer-data-febox">
+            <div class="footer-content-de-febox">
+                <a href="https://febox.ru" target="_blank">
+                    <svg viewBox="0 0 116.2 116.2"><path class="st0" d="M0,0v116.2h116.2V0H0z M46.5,63h-15v11.8H19.7V54.2h26.8V63z M55.2,51.5H19.7v-9h35.5V51.5z M96,74.7H60.5V54.2 h26.8V63h-15v2.7H96V74.7z M95.5,51.5H60v-9h35.5V51.5z"></path></svg>
+                    <span><i>Разработка сайта</i><b></b></span>
+                </a>
+
+                <span>Все материалы сайта являются <br>объектами авторского права</span>
+            </div>
+        </div>
+    </div>
+</footer>
+</body>
+</html>
